fc7df6fd5f68a8a9b654d67567a1cf699cd5365d
Sodex234 <sodex.gaming@gmail.com>
2018-11-17 13:24:41 +0000

    Added basic Vector stuff

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,12 +1,6 @@
 #include "stdafx.h"
 #include "Vector3.h"
 
-
-Vector3::Vector3()
-{
-}
-
-
-Vector3::~Vector3()
-{
-}
+Vector3::Vector3(double x, double y, double z)
+	:x{x}, y{y}, z{z}
+{}
--- Vector3.h
+++ Vector3.h
@@ -2,7 +2,14 @@
 class Vector3
 {
 public:
-	Vector3();
+	Vector3(double x, double y, double z);
 	~Vector3();
+	double get_x() const;
+	double get_y() const;
+	double get_z() const;
+private:
+	double x;
+	double y;
+	double z;
 };
 
